047147c8631537088d0928b08f58c58110e8d4e8
Joshua Martin <macdemarco@Joshuas-iMac.local>
2017-06-16 12:26:22 -0500

    Initial commit

---
+++ WaxHead/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaxHead
+//
+//  Created by Joshua Martin on 6/16/17.
+//  Copyright © 2017 Joshua Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
